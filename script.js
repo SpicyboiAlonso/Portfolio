12d248ca358380facd6a4b62afd3439fd674a369
Alonso Sanchez <Alonsosanchez2165@gmail.com>
2021-03-12 13:24:14 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+$(function(){
+    $(`.tabs-panels .tabs li`).on('click', function() {
+        let $panel = $(this).closest(`.tabs-panels`);
+        
+        $panel.find(`.tabs li.active`).removeClass('active');
+        $(this).addClass(`active`);
+
+        // find the panel we want to show
+
+        let panelToShow = $(this).attr('rel')
+
+        // hide the current one 
+
+        $panel.find(`.panel.active`).slideUp(300, showNextPanel)
+
+        // show the next aonel
+
+        function showNextPanel(){
+            $(this).removeClass(`active`)
+        
+            $(`#${panelToShow}`).slideDown(300, function(){
+                $(this).addClass(`active`)
+            })
+        }
+    })
+})
